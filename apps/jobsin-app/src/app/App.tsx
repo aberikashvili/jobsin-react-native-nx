c647386aa50dc0eb02e03ffccace7b6f5682fbc3
kethrin7 <ketevaniburjanadze@gmail.com>
2023-11-08 01:08:34 +0400

    [Jobsin-31] implemented small card template

--- apps/jobsin-app/src/app/App.tsx
+++ apps/jobsin-app/src/app/App.tsx
@@ -7,33 +7,10 @@ export const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-<<<<<<< HEAD
       <StackNavigator />
-=======
-      <View style={styles.container}>
-        <Text style={styles.text}>App</Text>
-      </View>
->>>>>>> 224a7df (add functionality to close the modal by pressing outside of the Modal.)
     </>
 
   );
 };
 
-<<<<<<< HEAD
-=======
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    ...Typography({
-      weight: 'bold',
-    }),
-    textAlign: 'center',
-  },
-});
-
->>>>>>> 224a7df (add functionality to close the modal by pressing outside of the Modal.)
 export default App;
