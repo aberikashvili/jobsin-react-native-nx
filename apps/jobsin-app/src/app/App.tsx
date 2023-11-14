0becaa3b187f3b1c2fa30d418d3b68c6a6b3a9db
Gvn <gvanca.uplisashvili@gmail.com>
2023-11-14 13:13:05 +0400

    Custom Salary Range Slider with Dynamic Shadow
    - A dynamic shadow effect that visually represents the selected salary
    range. This shadow adjusts in width and position based on the slider
    values, enhancing user interaction.
    - Custom thumb labels displaying the current values in a clear and concise
    format, making it easier for users to determine their selected range.
    - Responsive design and style adjustments to ensure the component is
    visually appealing and functional across different device sizes.

--- apps/jobsin-app/src/app/App.tsx
+++ apps/jobsin-app/src/app/App.tsx
@@ -6,7 +6,7 @@ export const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <View>
+      <View style={styles.container}>
         <Text style={styles.text}>App</Text>
       </View>
     </>
@@ -14,10 +14,30 @@ export const App = () => {
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
   text: {
     ...Typography({
       weight: 'bold',
     }),
+  },
+  sliderContainer: {
+    width: '100%',
+    padding: 20,
+    backgroundColor: '#FFF',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 6,
+    shadowOpacity: 0.1,
+    elevation: 4,
+    borderRadius: 10,
   },
 });
 
