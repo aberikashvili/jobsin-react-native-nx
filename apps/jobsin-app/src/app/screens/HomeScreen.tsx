93045588cce751fc78e0007fc3536b64032294d9
GeorgeKhareX <kharaishvili50@gmail.com>
2023-11-23 00:32:33 +0400

    add-bottom-sheet

--- apps/jobsin-app/src/app/screens/HomeScreen.tsx
+++ apps/jobsin-app/src/app/screens/HomeScreen.tsx
@@ -1,11 +1,27 @@
-import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import { useState } from 'react';
+
+import { Button, SafeAreaView, StyleSheet, Text } from 'react-native';
 
 import { Typography } from '@libs/constants';
+import { Bottom } from '@libs/core';
 
 const HomeScreen = () => {
+  const [bottomSheetVisible, setBottomSheetVisible] = useState(false);
+
   return (
     <SafeAreaView style={styles.container}>
+      <Button
+        title="Open Bottom Sheet"
+        onPress={() => setBottomSheetVisible(true)}
+      />
       <Text style={styles.text}>HomeScreen</Text>
+      <Bottom
+        isVisible={bottomSheetVisible}
+        height={300}
+        onClose={() => setBottomSheetVisible(false)}
+      >
+        <Text>This is the content of the bottom sheet.</Text>
+      </Bottom>
     </SafeAreaView>
   );
 };
--- libs/core/src/index.ts
+++ libs/core/src/index.ts
@@ -1,2 +1,3 @@
 export * from './button/Button';
 export * from './checkbox/CheckBox';
+export * from './lib/bottom-sheet/Bottom';
