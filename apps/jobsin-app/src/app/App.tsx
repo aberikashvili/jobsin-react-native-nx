1fd8a35c1d5428d547692d5982c1b0bb3c3023fe
Gvn <gvanca.uplisashvili@gmail.com>
2023-11-22 14:13:01 +0400

    fixed icon comp

--- apps/jobsin-app/src/app/App.tsx
+++ apps/jobsin-app/src/app/App.tsx
@@ -13,3 +13,18 @@ export const App = () => {
 };
 
 export default App;
+
+import { StatusBar } from 'react-native';
+
+import StackNavigator from './navigators/StackNavigator';
+
+export const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <StackNavigator />
+    </>
+  );
+};
+
+export default App;
