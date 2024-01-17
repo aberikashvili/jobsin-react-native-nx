02bff589284d44883dbf4f0ad7dd4de83085630b
GeorgeKhareX <kharaishvili50@gmail.com>
2024-01-18 00:28:21 +0400

    [defect]-18.00.28

--- apps/jobsin-app/src/app/screens/HomeScreen.tsx
+++ apps/jobsin-app/src/app/screens/HomeScreen.tsx
@@ -1,27 +1,36 @@
 import { useState } from 'react';
 
-import { Button, SafeAreaView, StyleSheet, Text } from 'react-native';
+import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 import { Typography } from '@libs/constants';
-import { Bottom } from '@libs/core';
+import { Bottom, Picture } from '@libs/core';
+
+import { UseStore } from '../store/JobsData';
 
 const HomeScreen = () => {
   const [bottomSheetVisible, setBottomSheetVisible] = useState(false);
+  const { data } = UseStore();
 
   return (
     <SafeAreaView style={styles.container}>
-      <Button
-        title="Open Bottom Sheet"
-        onPress={() => setBottomSheetVisible(true)}
-      />
-      <Text style={styles.text}>HomeScreen</Text>
-      <Bottom
-        isVisible={bottomSheetVisible}
-        height={300}
-        onClose={() => setBottomSheetVisible(false)}
-      >
-        <Text>This is the content of the bottom sheet.</Text>
-      </Bottom>
+      <View style={styles.header}>
+        <Picture circle={true} width={50} height={50} source={data[0].image} />
+        <Text style={styles.text}>{data[0].username}</Text>
+      </View>
+      <View style={styles.bottomSheet}>
+        <Button
+          title="Open Bottom Sheet"
+          onPress={() => setBottomSheetVisible(true)}
+        />
+        <Text style={styles.text}>HomeScreen</Text>
+        <Bottom
+          isVisible={bottomSheetVisible}
+          height={300}
+          onClose={() => setBottomSheetVisible(false)}
+        >
+          <Text>This is the content of the bottom sheet.</Text>
+        </Bottom>
+      </View>
     </SafeAreaView>
   );
 };
@@ -32,9 +41,12 @@ const styles = StyleSheet.create({
   container: {
     display: 'flex',
     alignItems: 'center',
+    justifyContent: 'center',
     marginHorizontal: 8,
     marginVertical: 12,
   },
+  header: {},
+  bottomSheet: {},
   text: {
     ...Typography({
       weight: 'bold',
